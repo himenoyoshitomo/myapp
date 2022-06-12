fffa7502079dba6c62d446d7333812ad00652c66
Yoshitomo Himeno <v21e3017@oita-u.ac.jp>
2022-06-12 03:20:53 +0000

    Lesson08の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
     
+    def index
+        @posts=Post.all.order(created_at:'desc')
+    end
+    
     def show
         @posts=Post.find(params[:id])
     end
     
-    def index
-        @posts=Post.all.order(created_at:'desc')
-    end
+    def new
+    end 
+
 end
