12e8b31656a525764dd4c0d13184bcd7cc05c4f7
Yoshitomo Himeno <v21e3017@oita-u.ac.jp>
2022-06-18 00:32:58 +0000

    Lesson09の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,25 @@ class PostsController < ApplicationController
         @posts=Post.find(params[:id])
     end
     
+    
+    def create
+        #render plain:"受信パラメータ:#{params}"
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render "new"
+        end
+    end
+    
     def new
+        @post = Post.new
     end 
+    
+private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 
 end
