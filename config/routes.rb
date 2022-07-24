e5d263e47d4e2e2a195864a38490683fbfb0a91f
Yoshitomo Himeno <v21e3017@oita-u.ac.jp>
2022-07-24 01:36:41 +0000

    Lesson14の課題

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :sessions
   
   root 'posts#index'
 end
